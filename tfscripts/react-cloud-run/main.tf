b1a4eec455cf8541d79ab13c5596e55e14216b01
Rahul Abrol <rahul.abrol@hcl.com>
2021-06-05 08:31:41 +0000

    trigger

---
+++ tfscripts/react-cloud-run/main.tf
@@ -0,0 +1,34 @@
+provider "google" {
+  version = "3.5.0"
+  project = "${var.project_id}"
+}
+
+resource "google_cloud_run_service" "default" {
+  name     = "cloudrun-opt-out"
+  location = "${var.region}"
+
+  template {
+    spec {
+      containers {
+        image = "${var.docker_image}"
+      }
+    }
+  }
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.default.location
+  project     = google_cloud_run_service.default.project
+  service     = google_cloud_run_service.default.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
