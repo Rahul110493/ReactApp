0e3d60bc5d195e62d4cd8f837d693dbfb2e49ac4
Rahul110493 <38890875+Rahul110493@users.noreply.github.com>
2021-06-08 19:44:00 +0530

    Update main.tf

--- tfscripts/main.tf
+++ tfscripts/main.tf
@@ -25,7 +25,11 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   location = "${var.container_node_pool_name_location_id}"
   cluster    = google_container_cluster.primary.name
   node_count = "${var.node_count}"
- 
+  
+  node_locations = [
+    "${var.container_node_pool_name_location_id}",
+  ]
+  
 node_config {
 
     # preemptible  = true 
