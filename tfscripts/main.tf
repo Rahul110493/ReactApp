8da323abbc32aaa36227e2fa30d4fb6ae5af2925
Rahul110493 <38890875+Rahul110493@users.noreply.github.com>
2021-06-09 11:59:41 +0530

    Update main.tf

--- tfscripts/main.tf
+++ tfscripts/main.tf
@@ -9,11 +9,7 @@ resource "google_container_cluster" "primary" {
  
   name  = "${var.cls_name}"
   location = "${var.cls_location_id}"
- # We can't create a cluster with no node pool defined, but we want to only use
- # separately managed node pools. So we create the smallest possible default
-#  node pool and immediately delete it.
-#   remove_default_node_pool = true
-#   initial_node_count       = 1
+
   remove_default_node_pool = "${var.remove_default_node_pool}"
   initial_node_count    = "${var.initial_node_count}"
   network    = google_compute_network.vpc.name
@@ -26,14 +22,8 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   cluster    = google_container_cluster.primary.name
   node_count = "${var.node_count}"
   
-#   node_locations = [
-#     "${var.container_node_pool_name_location_id}",
-#   ]
   
 node_config {
-
-    # preemptible  = true 
-    # machine_type = "e2-medium"
 
     preemptible  = "${var.preemptible}"  
     machine_type = "${var.machine_type}"
